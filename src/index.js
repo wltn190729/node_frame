fd93391fed80e1f2bfbe335cd2f64a6831b4216a
정수진 <xbfflq403@naver.com>
2022-12-07 10:44:41 +0900

    feat: :zap: port : 7777

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /** 서버 실행할 포트 설정 **/
-const port = 3000;
+const port = 7777;
 
 /** 필요한 의존성 모듈 로드 **/
 const express = require("express"),
-    bodyParser = require("body-parser"),
-    cors = require("cors"),
-    routes = require("./routes"),
-    fileUpload = require("express-fileupload");
+  bodyParser = require("body-parser"),
+  cors = require("cors"),
+  routes = require("./routes"),
+  fileUpload = require("express-fileupload");
 
 /** Helper Load **/
 require("../Helpers/DateFormat.helper");
@@ -31,10 +31,8 @@ app.use(
   })
 );
 
-
 /** 라우터 설정 불러오기 **/
-app.use(routes)
-
+app.use(routes);
 
 /** 서버 실행
  * nodemon을 통해서 npm run server 입력 시 서버 실행됨.**/
